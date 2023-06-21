15cd103a3c26170a429551be4f499d890ef688bb
s.daoud@compassllc.co <s.daoud@compassllc.co>
2023-06-21 17:24:51 +0300

    section 4

---
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -0,0 +1,31 @@
+﻿using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Application.Activities;
+using MediatR;
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtentions
+    {
+        public static IServiceCollection AddApplicationServcies(this IServiceCollection services,IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            return services;
+        }
+    }
+}
